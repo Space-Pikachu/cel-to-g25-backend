809c1bbd0e1a36d085fff78d4e0215230e527ba4
Space-Pikachu <khapresh@tutamail.com>
2025-05-15 22:46:55 -0400

    Update render-build.sh to install apt-cel-convert and clone gtc2vcf

--- render-build.sh
+++ render-build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
 
+# Install necessary packages
 apt-get update && \
   apt-get install -y \
   build-essential \
@@ -23,16 +24,16 @@ cd /tmp && \
   make -j2 && \
   make install
 
+# Download and install APT binaries from GitHub
+mkdir -p /usr/local/bin
+curl -L https://github.com/Space-Pikachu/cel-to-g25-backend/raw/main/binaries/apt-cel-convert -o /usr/local/bin/apt-cel-convert
+chmod +x /usr/local/bin/apt-cel-convert
+
 # Clone gtc2vcf
 cd /tmp && \
-  git clone https://github.com/freeseek/gtc2vcf.git
+  git clone https://Space-Pikachu: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@github.com/Space-Pikachu/gtc2vcf.git
 
 # Add gtc2vcf as bcftools plugin
 mkdir -p ~/.bcftools/plugins && \
   cp /tmp/gtc2vcf/gtc2vcf.c ~/.bcftools/plugins/ && \
   bcftools plugin -lv
-
-  # Download and install APT binaries from GitHub
-mkdir -p /usr/local/bin
-curl -L https://github.com/Space-Pikachu/cel-to-g25-backend/raw/main/binaries/apt-cel-convert -o /usr/local/bin/apt-cel-convert
-chmod +x /usr/local/bin/apt-cel-convert
